78524aaf227bc19960d238b027316f6b503dfbac
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-12-19 11:54:55 -0500

    update

--- tipp.sh
+++ tipp.sh
@@ -1,1 +1,1 @@
-ls -S dst | xargs -n 1 -I "{}" echo tippecanoe --minimum-zoom=10 --maximum-zoom=15 --base-zoom=15 --read-parallel -f --simplification=2 -o mbtiles/"{}".mbtiles dst/"{}"
+ls -S dst | xargs -n 1 -I "{}" echo tippecanoe --drop-densest-as-needed --minimum-zoom=10 --maximum-zoom=15 --base-zoom=15 --read-parallel -f --simplification=2 -o mbtiles/"{}".mbtiles dst/"{}"
